917efc0725d2ba66a7f0240a1a615f06b884704e
CharSui <798296184@qq.com>
2025-02-18 18:24:58 +0800

    First Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# CharSui's Texture Previewer
+
+[Chinese](README.md)
+[English](README-EN.md)
+
+Unity默认的Inspector在查看贴图的时候没办法放大，但是又想要看看经过压缩设置之后的贴图精度怎么样，遂让DeepSeek生成代码。
+
+# 怎么用？
+## 通过文件选择器加载
+
+
+
+
+## 通过Project界面加载
+
---
+++ Samples~/InspectorExtension/Editor/TextureInsepctorExtension.cs
@@ -0,0 +1,56 @@
+﻿/*
+ * Author: CharSui
+ * Created On: 2025.02.18
+ * Description: 【代码借助Deepseek-r1生成】在Inspector中添加快速打开预览窗口的方式
+ */
+
+using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace CharSui.TexturePreviewer.EditorExtension
+{
+	[CustomEditor(typeof(TextureImporter))]
+	public class TextureInspectorExtension : Editor
+	{
+		private Editor defaultEditor;
+
+		private void OnEnable()
+		{
+			// 创建默认的TextureImporter编辑器
+			defaultEditor = CreateEditor(targets, Type.GetType("UnityEditor.TextureImporterInspector, UnityEditor"));
+		}
+
+		private void OnDisable()
+		{
+			// 销毁默认编辑器
+			DestroyImmediate(defaultEditor);
+		}
+
+		public override void OnInspectorGUI()
+		{
+			// 显示默认界面
+			defaultEditor.OnInspectorGUI();
+
+			// 只对Texture2D类型生效
+			EditorGUILayout.Space(20);
+			if (GUILayout.Button("预览", GUILayout.Height(40)))
+			{
+				var importer = target as TextureImporter;
+				if (importer == null)
+				{
+					return;
+				}
+
+				var texture = AssetDatabase.LoadAssetAtPath<Texture>(importer.assetPath);
+				HandleTexture(texture);
+			}
+		}
+
+		private void HandleTexture(Texture texture)
+		{
+			// 示例：打开自定义窗口
+			TexturePreviewer.PreviewTexture(texture);
+		}
+	}
+}
